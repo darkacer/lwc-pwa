1f620c13a0acc308ab9f7f0c81b6199994ef763e
darkacer <omkardeokar95@gmail.com>
2021-01-08 15:16:09 +0530

    adding notification

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -3,15 +3,40 @@ import { LightningElement } from 'lwc';
 const CONTACTS_URL = 'api/contacts';
 
 export default class App extends LightningElement {
-
     contacts = [];
+    notifStatus = '';
 
     connectedCallback() {
-        fetch(CONTACTS_URL).then(response => {
-            return response.json();
-        }).then( contacts => {
-            this.contacts = contacts;
+        fetch(CONTACTS_URL)
+            .then((response) => {
+                return response.json();
+            })
+            .then((contacts) => {
+                this.contacts = contacts;
+            });
+
+        Notification.requestPermission((status) => {
+            console.log('status is  ', status);
+            this.notifStatus = status;
         });
     }
 
+    showNotification() {
+        let options = {
+            body: 'Notif Body',
+            vibrate: [100, 50, 100],
+            data: { primaryKey: 1 }
+        };
+        if (Notification.permission === 'granted') {
+            navigator.serviceWorker.getRegistration().then((reg) => {
+                reg.showNotification('Hello Notification', options);
+            });
+        }
+    }
+
+    subscribeNotifcation() {
+        Notification.requestPermission((status) => {
+            console.log('status is  ', status);
+        });
+    }
 }
