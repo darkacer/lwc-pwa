43c629a9e40c745e2c3143fd9b1dd1f8e88a3471
darkacer <omkardeokar95@gmail.com>
2021-01-26 22:39:27 +0530

    added routing

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,26 +1,95 @@
-import { LightningElement } from 'lwc';
-
-const CONTACTS_URL = 'api/contacts';
+import { LightningElement, track } from 'lwc';
+import Navigo from 'navigo';
+// const router = new Navigo('/', false);
+// const CONTACTS_URL = 'api/contacts';
 
 export default class App extends LightningElement {
     contacts = [];
     notifStatus = '';
+    roomname = 'publicroom1';
+    menuOpen = false;
+    router = new Navigo('/', false);
 
-    connectedCallback() {
-        fetch(CONTACTS_URL)
-            .then((response) => {
-                return response.json();
-            })
-            .then((contacts) => {
-                this.contacts = contacts;
-            });
+    @track view = {};
 
-        Notification.requestPermission((status) => {
-            console.log('status is  ', status);
-            this.notifStatus = status;
+    menuItems = [
+        {
+            title: 'Podcast',
+            location: '/podcasts',
+            iconName: 'grid'
+        },
+        {
+            title: 'Discover',
+            location: '/discover',
+            iconName: 'search'
+        },
+        {
+            title: 'Settings',
+            location: '/settings',
+            iconName: 'settings'
+        }
+    ];
+
+    constructor() {
+        super();
+        // fetch(CONTACTS_URL)
+        //     .then((response) => {
+        //         return response.json();
+        //     })
+        //     .then((contacts) => {
+        //         this.contacts = contacts;
+        //     });
+
+        // Notification.requestPermission((status) => {
+        //     console.log('status is  ', status);
+        //     this.notifStatus = status;
+        // });
+
+        this.router.on({
+            '/podcasts': async () => {
+                console.log('indie');
+                const { default: ViewChatWindow } = await import(
+                    'views/chatWindow'
+                );
+                this.setView(ViewChatWindow);
+            },
+            '/power': async () => {
+                console.log('indie');
+                const { default: ViewChatWindow } = await import(
+                    'views/chatWindow'
+                );
+                this.setView(ViewChatWindow);
+            }
         });
+
+        const navigateToDefault = () => {
+            this.router.navigate('/podcasts');
+        };
+
+        this.router.notFound(navigateToDefault);
+        this.router.on(navigateToDefault);
+
+        this.router.resolve();
     }
 
+    handleMenuItemClick(event) {
+        event.preventDefault();
+        const { href } = event.target;
+        console.log('before event', event.target.href, href);
+        this.router.navigate('/podcasts', true);
+        // this.template.querySelector('base-menu').close();
+    }
+
+    setView(component, props = {}) {
+        this.view = {
+            component,
+            props
+        };
+    }
+
+    toogleMenu() {
+        this.menuOpen = !this.menuOpen;
+    }
     showNotification() {
         let options = {
             body: 'Notif Body',
---
+++ src/modules/views/chatWindow/chatWindow.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class ViewChatWindow extends LightningElement {
+    @api props;
+}
